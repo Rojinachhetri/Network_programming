dae22fe2a75d2731a34c1ad243277c0afbe1b4ff
Rojina Chheteri <chhetrirojina247@gmail.com>
2024-06-02 07:29:28 +0545

    updated

--- unit2/IpByDomain.java
+++ unit2/IpByDomain.java
@@ -1,10 +1,13 @@
+// A program to print the address of www.google.com
 package unit2;
 import java.net.*;
 public class IpByDomain {
     public static void main(String[] args) {
         try{
             InetAddress address = InetAddress.getByName("www.google.com");
-            System.out.println(address);
+            System.out.println("Address: "+ address.getHostAddress());
+            System.out.println("Name: "+ address.getHostName());
+
         }
         catch (UnknownHostException ex){
             System.out.println("could not find www.google.com");
