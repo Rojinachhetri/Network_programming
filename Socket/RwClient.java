37b19eb9d83c74bc13a3c8c38c2ad938865bee05
Rojina Chheteri <chhetrirojina247@gmail.com>
2024-07-03 07:21:59 +0545

    communication

---
+++ Socket/RwClient.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.net.Socket;
+
+public class RwClient {
+    public static void main(String[] args) {
+        try (Socket client = new Socket("localhost", 6667);
+             DataOutputStream dout = new DataOutputStream(client.getOutputStream());
+             DataInputStream din = new DataInputStream(client.getInputStream());
+             BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+
+            String s1 = "", s2 = "";
+
+            while (!s1.equals("q")) {
+                System.out.print("Enter message for server: ");
+                s2 = reader.readLine();
+                dout.writeUTF(s2);
+                dout.flush();
+
+                s1 = din.readUTF();
+                System.out.println("Server says: " + s1);
+            }
+            dout.close();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
---
+++ Socket/RwServer.java
@@ -0,0 +1,36 @@
+
+import java.io.*;
+import java.net.*;
+
+public class RwServer {
+
+    public static void main(String[] args) {
+        try {
+            ServerSocket ss = new ServerSocket(6667);
+            Socket server = ss.accept();
+            // din and dout for socket read and write
+            DataInputStream din = new DataInputStream(server.getInputStream());
+            DataOutputStream dout = new DataOutputStream(server.getOutputStream());
+            // read from standard input device
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+            String s1="" ,s2="";
+            while(!s1.equals("q")){
+                s1 = din.readUTF();
+                System.out.println("Client says: " + (String) s1 );
+                System.out.print("Enter message for Client: ");
+
+                s2 = reader.readLine();
+                dout.writeUTF(s2);
+                dout.flush();
+                // dout.close();
+            }
+
+            // din close
+            ss.close();
+            System.out.println("Connection Established");
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
