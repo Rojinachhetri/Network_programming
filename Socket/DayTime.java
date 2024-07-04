d6ea6cc49d14ec13cce87b2be2c7bdf50c59929f
Rojina Chheteri <chhetrirojina247@gmail.com>
2024-07-04 07:36:33 +0545

    dictionary

--- Socket/DayTime.java
+++ Socket/DayTime.java
@@ -6,7 +6,7 @@ public class DayTime {
     public static void main(String[] args) {
         int ch;
         try {
-            // Correct the URL to use the hostname and port for the Daytime Protocol
+            
             Socket socket = new Socket("time.nist.gov", 13);
             System.out.println(socket.toString());
             InputStream in = socket.getInputStream();
